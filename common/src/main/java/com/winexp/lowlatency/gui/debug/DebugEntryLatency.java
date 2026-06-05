bbab9b46798676b1627c431587bda78ad52c1fbb
WinExp <winexp2333@qq.com>
2026-06-05 22:09:23 +0800

    refactor: optimize debug entry text

--- common/src/main/java/com/winexp/lowlatency/gui/debug/DebugEntryLatency.java
+++ common/src/main/java/com/winexp/lowlatency/gui/debug/DebugEntryLatency.java
@@ -14,9 +14,9 @@ public class DebugEntryLatency implements DebugScreenEntry {
     public void display(DebugScreenDisplayer displayer, Level level, LevelChunk clientChunk, LevelChunk serverChunk) {
         String status = LowLatencyMod.CONFIG.enabled ? "enabled" : "disabled";
         displayer.addToGroup(MAIN, String.format("Low latency: %s", status));
-        displayer.addToGroup(MAIN, String.format("Average CPU Time: %.4f ms", LowLatencyMod.SCHEDULER.getAverageCpuTime() / 1_000_000.0));
-        displayer.addToGroup(MAIN, String.format("Average GPU Time: %.4f ms", LowLatencyMod.SCHEDULER.getAverageGpuTime() / 1_000_000.0));
-        displayer.addToGroup(MAIN, String.format("Estimated Latency: %.4f ms", LowLatencyMod.SCHEDULER.getEstimatedLatency() / 1_000_000.0));
-        displayer.addToGroup(MAIN, String.format("Queue length: %d", LowLatencyMod.SCHEDULER.getQueueLength()));
+        displayer.addToGroup(MAIN, String.format("Average CPU time: %.4f ms", LowLatencyMod.SCHEDULER.getAverageCpuTime() / 1_000_000.0));
+        displayer.addToGroup(MAIN, String.format("Average GPU time: %.4f ms", LowLatencyMod.SCHEDULER.getAverageGpuTime() / 1_000_000.0));
+        displayer.addToGroup(MAIN, String.format("Estimated latency: %.4f ms", LowLatencyMod.SCHEDULER.getEstimatedLatency() / 1_000_000.0));
+        displayer.addToGroup(MAIN, String.format("Frame queue length: %d", LowLatencyMod.SCHEDULER.getQueueLength()));
     }
 }
